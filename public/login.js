253c76ea7a1cef8e0632e597600e329f9fec08aa
JAGeorge54 <jesse.george54@gmail.com>
2024-04-04 15:27:09 -0400

    added user balance to context

--- public/login.js
+++ public/login.js
@@ -40,7 +40,8 @@ function LoginForm(props){
             props.setShow(false);
             console.log('JSON:', data);
             ctx[3].setlogIn(true)
-            ctx[1].idUser(data.email)
+            ctx[1].idUser(data)
+            ctx[5].balanceUser(data.balance)
         } catch(err) {
             props.setStatus(text)
             console.log('err:', text);
