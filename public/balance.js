92eb0ae78eadcfbacf7a90a90ba3314b2801a279
JAGeorge54 <jesse.george54@gmail.com>
2024-04-08 15:28:17 -0400

    changed user to store all user data

--- public/balance.js
+++ public/balance.js
@@ -55,7 +55,7 @@ function BalanceForm(props){
   return (<>
 
     Balance<br/>
-    {ctx[0].user ? ctx[4].userBal.balance : 'not logged in'}<br/>
+    {ctx[0].user ? ctx[0].user.balance : 'not logged in'}<br/>
 
   </>);
 }
